493969d11f83bca74c822444d9cf87faa80a5652
brray94 <79230999+brray94@users.noreply.github.com>
2021-06-04 22:01:39 -0500

    fuck
    intento fuckx2

--- app/src/main/java/com/h4lfcr3st/androidswissarmy/grafoss.java
+++ app/src/main/java/com/h4lfcr3st/androidswissarmy/grafoss.java
@@ -11,22 +11,23 @@ import android.widget.Toast;
 
 public class grafoss extends AppCompatActivity {
     private EditText valorMatriz, cantNodos;
-    private TextView matriz, infoValorMatriz, matrizT;
+    private TextView matriz, infoValorMatriz, resultado;
     private Button btnAgregar;
 
-    String acumulador="",acumuladorT="";
+    String acumulador="",acumuladorT="",loop="";
     int acumRep=1, countMatriz=0;
-    int sumaFilas = 0, sumaColumnas = 0, noDirigido = 0, acumColumna = 0;
+    int sumaFilas = 0, sumaColumnas = 0, noDirigido = 0, acumColumna = 0,numNodos=0,i=0,j=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_grafoss);
 
         valorMatriz = findViewById(R.id.txtnum_valor);
         cantNodos = findViewById(R.id.txt_numNodos);
         matriz = findViewById(R.id.tv_matriz);
-        matrizT = findViewById(R.id.tv_matrizT);
+        resultado = findViewById(R.id.tv_resultMatriz);
         infoValorMatriz = findViewById(R.id.txt_infoValorMatriz);
         valorMatriz.setEnabled(false);
         btnAgregar = findViewById(R.id.btn_agregarValor);
@@ -36,71 +37,87 @@ public class grafoss extends AppCompatActivity {
 
     public void dimensionMatriz(View view) {
         String dimMatriz = String.valueOf(cantNodos.getText());
-        while (dimMatriz.equals("")){
-            cantNodos.getError();
+        if (dimMatriz.equals("")){
+            cantNodos.setError("Requerido");
+        }else{
+            valorMatriz.setEnabled(true);
+            btnAgregar.setEnabled(true);
+            matriz.setText("");
+            acumulador="";
+            acumuladorT="";
+            acumRep=0;
+            countMatriz=0;
+            resultado.setText("");
+            sumaFilas = 0;
+            sumaColumnas = 0;
+            loop="";
+            i=0;
+            j=0;
         }
-        valorMatriz.setEnabled(true);
-        btnAgregar.setEnabled(true);
-        matriz.setText("");
-        acumulador="";
-        acumuladorT="";
-        acumRep=1;
-        countMatriz=0;
-        matrizT.setText("");
-        sumaFilas = 0;
-        sumaColumnas = 0;
     }
+    public void datosGrafo (View view){
 
-
-    public void agregarValor (View view) {
-
-        int numNodos = Integer.parseInt(String.valueOf(cantNodos.getText()));
-
-        String loop = "";
-
+        numNodos = Integer.parseInt(String.valueOf(cantNodos.getText()));
         int numeros[][] = new int[numNodos][numNodos];// primero filas y luego columnas
-        int numerosT[][] = new int[numNodos][numNodos];
-        String verificador = String.valueOf(valorMatriz.getText());
-        if (verificador.equals("")) {
-            valorMatriz.getError();
-        } else {
-            //infoValorMatriz.setText("Ingrese valor para la posición");
-            int valor = Integer.parseInt(String.valueOf(valorMatriz.getText()));
-            while (valor < 0) {
-                //infoValorMatriz.setText("Debe de ingresar un valor positivo");
-                valorMatriz.setText("");
-                valorMatriz.getError();
-                valor = Integer.parseInt(String.valueOf(valorMatriz.getText()));
-            }
+        //int numerosT[][] = new int[numNodos][numNodos];
+        String verificadorValor = String.valueOf(valorMatriz.getText());
 
-            if (acumRep==numNodos){
+        if (verificadorValor.equals("")){
+            valorMatriz.setError("Requerido");
+        }else{
+            int valor = Integer.parseInt(String.valueOf(valorMatriz.getText()));
+
+            if (i<numNodos){
+                if (j<numNodos){
+                    numeros[i][j]=valor;
+                }else{
+                    i++;
+                    j=0;
+                    numeros[i][j]=valor;
+                }
+                j++;
+            }
+            acumRep++;
+            if (acumRep==numNodos*numNodos){
+                for (int i = 0; i < numNodos; i++) {
+                    for (int j = 0; j < numNodos; j++) {
+                        acumulador = acumulador + "[" + numeros[i][j] + "]";
+                    }
+                    acumulador = acumulador + "\n";
+                }
+                resultado.setText(acumulador);
+            }
+            valorMatriz.setText("");
+            Toast.makeText(this, "Has agregado un valor", Toast.LENGTH_SHORT).show();
+
+            /*if (acumRep==numNodos){
                 acumRep=1;
                 numeros[countMatriz][acumRep]=valor;
-                //acumulador = acumulador + "[" + valor + "]"+ "\n";
                 acumulador = acumulador + "[" + numeros[countMatriz][acumRep] + "]"+ "\n";
                 countMatriz++;
                 matriz.setText(acumulador);
                 valorMatriz.setText("");
             }else{
                 numeros[countMatriz][acumRep]=valor;
-                //acumulador = acumulador + "[" + valor + "]";
                 acumulador = acumulador + "[" + numeros[countMatriz][acumRep] + "]";
                 acumRep++;
                 matriz.setText(acumulador);
                 valorMatriz.setText("");
             }
+            if (countMatriz==acumRep){
+                if (valor!=0){
+                    loop = loop + "Hay un loop en " + "[" + countMatriz + "]" + "[" + acumRep + "]\n";
+                }
+            }
             if (countMatriz==numNodos){
                 valorMatriz.setEnabled(false);
                 btnAgregar.setEnabled(false);
                 Toast.makeText(this, "Esa es su matriz", Toast.LENGTH_LONG).show();
-            }
-            //falta por corregir esta parte
-            if (acumRep<numNodos){
-                sumaFilas=sumaFilas+valor;
-            }else{
-                sumaColumnas=sumaColumnas+valor;
-            }
-            matrizT.setText("Grado de entrada: "+ sumaColumnas + "\nGrado de salida: " + sumaFilas);
+                resultado.setText("Resultados: " + loop);
+            }*/
+
         }
+
     }
+
 }
